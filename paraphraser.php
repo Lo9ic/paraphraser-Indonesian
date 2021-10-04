81860f5c68bfb0a67f19d6753ffb572afa66cf87
lo9ic <31589259+lo9ic@users.noreply.github.com>
2021-10-04 18:48:05 +0700

    Create paraphraser.php

---
+++ paraphraser.php
@@ -0,0 +1,69 @@
+<?php
+error_reporting(E_ERROR);
+function request($url, $data = null, $headers = null)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if($data):
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    endif;
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if($headers):
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+    endif;
+
+    curl_setopt($ch, CURLOPT_ENCODING, "GZIP");
+    return curl_exec($ch);
+}
+
+function kirim_telegram($message)
+{
+    $token = "1473238136:AAHzAXwr1dQnMFrkcrty-pCvK53YmhM_vUo"; // Isi secret tokennya
+    $chatIds = "685065835"; // Isi id telegramnya
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatIds;
+    $url = $url . "&text=" . urlencode($message);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+function getstr($str, $exp1, $exp2)
+{
+    $a = explode($exp1, $str)[1];
+    return explode($exp2, $a)[0];
+}
+
+echo "Teks : ";
+$text = trim(fgets(STDIN));
+
+$url = "https://www.paraphraser.io/frontend/rewriteArticleBeta";
+$headers[] = "Cookie: ci_session=i42435vebmu6kknl8g4p8n3dg3fg339u;";
+$headers[] = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0";
+$headers[] = "Accept: */*";
+$headers[] = "Accept-Language: id,en-US;q=0.7,en;q=0.3";
+$headers[] = "Accept-Encoding: gzip, deflate";
+$headers[] = "Referer: https://www.paraphraser.io/id/parafrase-online";
+$headers[] = "Content-Type: application/x-www-form-urlencoded; charset=UTF-8";
+$headers[] = "X-Requested-With: XMLHttpRequest";
+$headers[] = "Te: trailers";
+$data = "data=$text&mode=1&lang=id&code=0";
+$paraphraser = request($url, $data, $headers);
+if(strpos($paraphraser, '"paraphrase"')!==false)
+{
+    $result = getstr($paraphraser, 'paraphrase":"','\n');
+    $result = str_replace("<b>", "\033[01;31m", $result);
+    $result = str_replace("<\/b>", "\033[0m", $result);
+    echo "\n\nResult : $result\n";
+}
+else
+{
+    echo "Eror Paraphrase\n";
+}
